800e1d535819bcab77f1e104917b1cf2b1fccf8d
Sowmya, Murali <sowmya.murali@cannytechsolutions.com>
2019-07-24 15:40:18 +0530

    added new api to pull columns for a table

--- src/main/java/com/canny/snowflakemigration/service/util/listTables.java
+++ src/main/java/com/canny/snowflakemigration/service/util/listTables.java
@@ -9,18 +9,31 @@ import java.sql.Statement;
 import com.canny.snowflakemigration.service.dto.MigrationProcessDTO;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class listTables {
-    public static String[] listTable(MigrationProcessDTO migrationProcessDTO) throws SQLException,ClassNotFoundException {
-        Connection con = DriverManager.getConnection(migrationProcessDTO.getSourceConnectionUrl(), migrationProcessDTO.getSourceConnectionUsername(), migrationProcessDTO.getSourceConnectionPassword());
+    public static String[] listTable(MigrationProcessDTO migrationProcessDTO) throws SQLException,ClassNotFoundException,NullPointerException {
+       // Connection con = DriverManager.getConnection(migrationProcessDTO.getSourceConnectionUrl(), migrationProcessDTO.getSourceConnectionUsername(), migrationProcessDTO.getSourceConnectionPassword());
+        //Statement stmt = con.createStatement();
+        Properties properties0 = new Properties();
+		properties0.put("user", migrationProcessDTO.getSourceConnectionUsername());
+		properties0.put("password", migrationProcessDTO.getSourceConnectionPassword());
+		properties0.put("db",migrationProcessDTO.getSourceConnectionDatabase());
+	    properties0.put("schema",migrationProcessDTO.getSourceConnectionSchema());	
+	    Connection con = DriverManager.getConnection(migrationProcessDTO.getSourceConnectionUrl(),properties0);
         Statement stmt = con.createStatement();
-        ResultSet rs1 = stmt.executeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.Tables WHERE TABLE_SCHEMA = '"+migrationProcessDTO.getSourceConnectionDatabase()+"';");
+		 
+  
+        ResultSet rs1 = stmt.executeQuery("SELECT a.TABLE_NAME,b.COLUMN_NAME as PrimaryKey FROM INFORMATION_SCHEMA.TABLES a JOIN INFORMATION_SCHEMA.COLUMNS b ON a.TABLE_NAME = b.TABLE_NAME AND b.COLUMN_KEY = 'PRI' AND a.TABLE_SCHEMA = b.TABLE_SCHEMA  WHERE a.TABLE_SCHEMA = '"+migrationProcessDTO.getSourceConnectionSchema()+"';");
         ArrayList tn = new ArrayList();
         while(rs1.next())
         {
-            tn.add(rs1.getString("TABLE_NAME"));
+        	String s1 = rs1.getString("TABLE_NAME");
+        	s1 =s1.concat("|");
+            tn.add(s1.concat(rs1.getString("PrimaryKey")));
         }
         String[] tableNames = (String[])tn.toArray(new String[tn.size()]);
         return tableNames;
     }
 }
+//SELECT * FROM _v_tables;
