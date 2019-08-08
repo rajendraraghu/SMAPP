ca171b5aabe1b611b836a974b6973dc1cc816780
rajendraraghu <rajendra.raghu@cannytechsolutions.com>
2019-08-08 16:16:27 +0530

    lang removal mapper dependency

--- src/main/java/com/canny/snowflakemigration/service/mapper/UserMapper.java
+++ src/main/java/com/canny/snowflakemigration/service/mapper/UserMapper.java
@@ -48,7 +48,7 @@ public class UserMapper {
             user.setEmail(userDTO.getEmail());
             user.setImageUrl(userDTO.getImageUrl());
             user.setActivated(userDTO.isActivated());
-            user.setLangKey(userDTO.getLangKey());
+            // user.setLangKey(userDTO.getLangKey());
             Set<Authority> authorities = this.authoritiesFromStrings(userDTO.getAuthorities());
             user.setAuthorities(authorities);
             return user;
