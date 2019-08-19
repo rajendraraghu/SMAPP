2be673844d751911d1c27a7d89560a9c5dc23980
rajendraraghu <rajendra.raghu@cannytechsolutions.com>
2019-08-19 18:30:35 +0530

    Login Issue Fix

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SnowpoleSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
+import { AccountService } from 'app/core';
 
 @NgModule({
   imports: [SnowpoleSharedCommonModule],
   declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
   entryComponents: [JhiLoginModalComponent],
   exports: [SnowpoleSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [AccountService]
 })
 export class SnowpoleSharedModule {
   static forRoot() {
