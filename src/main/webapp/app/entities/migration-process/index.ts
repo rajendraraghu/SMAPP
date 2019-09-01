bcc4e33937059bdeff710fd4eaa8d7c1ff38ff5f
rajendraraghu <rajendra.raghu@cannytechsolutions.com>
2019-09-01 17:16:34 +0530

    Migration Process fix

--- src/main/webapp/app/entities/migration-process/index.ts
+++ src/main/webapp/app/entities/migration-process/index.ts
@@ -4,4 +4,3 @@ export * from './migration-process-delete-dialog.component';
 export * from './migration-process-detail.component';
 export * from './migration-process.component';
 export * from './migration-process.route';
-export * from './migration-process-primary-key.component';
