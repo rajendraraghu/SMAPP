1d2316f43aa9ecf070efa5248d7ff8317123dbeb
rajendraraghu <rajendra.raghu@cannytechsolutions.com>
2019-09-05 19:14:24 +0530

    Adding Firstname and Lastname to Register page

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -22,6 +22,8 @@ export class RegisterComponent implements OnInit, AfterViewInit {
 
   registerForm = this.fb.group({
     login: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50), Validators.pattern('^[_.@A-Za-z0-9-]*$')]],
+    firstName: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50), Validators.pattern('^[_.@A-Za-z0-9-]*$')]],
+    lastName: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50), Validators.pattern('^[_.@A-Za-z0-9-]*$')]],
     email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
     password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
     confirmPassword: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]]
@@ -47,12 +49,14 @@ export class RegisterComponent implements OnInit, AfterViewInit {
   register() {
     let registerAccount = {};
     const login = this.registerForm.get(['login']).value;
+    const firstName = this.registerForm.get(['firstName']).value;
+    const lastName = this.registerForm.get(['lastName']).value;
     const email = this.registerForm.get(['email']).value;
     const password = this.registerForm.get(['password']).value;
     if (password !== this.registerForm.get(['confirmPassword']).value) {
       this.doNotMatch = 'ERROR';
     } else {
-      registerAccount = { ...registerAccount, login, email, password };
+      registerAccount = { ...registerAccount, login, firstName, lastName, email, password };
       this.doNotMatch = null;
       this.error = null;
       this.errorUserExists = null;
